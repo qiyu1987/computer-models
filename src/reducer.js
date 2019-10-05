2a8b80114a27923412c2cb3d0e1a9f97f082da66
qiyu1987 <qiyu1987@gmail.com>
2019-10-05 06:43:42 +0200

    handle redux action ADD_MODEL

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,7 @@
 const reducer = (state = [], action = {}) => {
     switch (action.type) {
+    case 'ADD_MODEL':
+        return [...state, {...action.payload}]
     default:
       return state
     }
